67f2c2115864a96592e821063d968ecf5544312b
NguyenQuocHoa <nguyenquochoa2000tb2@gmail.com>
2020-09-06 13:00:34 +0700

    code

---
+++ applications/demo/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "demo-app",
+
+    'summary': """"this is demo""",
+
+    'description': """"
+        No description
+    """,
+
+    'author': "My company",
+
+    'website': "url",
+
+    'category': 'Demo',
+    'version': '1',
+
+    'depends': ['base'],
+
+    'data': [
+        'template.xml',
+    ],
+    'demo': [
+        'demo.xml',
+    ],
+}
